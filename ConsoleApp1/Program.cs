c756b5a25eec0b18da9962c89e7da13aa595c7cd
xwalke[zd] <xwalke@gmail.com>
2017-04-14 01:03:14 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = Int16.MaxValue;
+            int x =  max + 1;
+            Console.WriteLine(x.ToString());
+            Console.ReadLine();
+        }
+    }
+}
